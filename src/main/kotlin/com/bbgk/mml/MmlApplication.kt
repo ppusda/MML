8aeb1b11555e157b6f17439522feed22a96f8b0c
ppusda <ppusda1234@gmail.com>
2024-10-02 09:51:46 +0900

    init: initialize project

---
+++ src/main/kotlin/com/bbgk/mml/MmlApplication.kt
@@ -0,0 +1,11 @@
+package com.bbgk.mml
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MmlApplication
+
+fun main(args: Array<String>) {
+	runApplication<MmlApplication>(*args)
+}
---
+++ src/test/kotlin/com/bbgk/mml/MmlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bbgk.mml
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MmlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
