3f1f3a8f1135a5b4b698222992e54c3c2a694709
ppusda <ppusda1234@gmail.com>
2024-10-15 04:00:18 +0900

    feat: DTO 개발

---
+++ src/main/kotlin/com/bbgk/mml/dto/MusicDTO.kt
@@ -0,0 +1,15 @@
+package com.bbgk.mml.dto
+
+import com.bbgk.mml.domain.entity.Music
+
+data class MusicDTO(
+        val title: String,
+        val artist: String,
+        val url: String
+) {
+    constructor(music: Music) : this (
+            title = music.title,
+            artist = music.artist,
+            url = music.url
+    )
+}
---
+++ src/main/kotlin/com/bbgk/mml/dto/PlaylistDTO.kt
@@ -0,0 +1,13 @@
+package com.bbgk.mml.dto
+
+import com.bbgk.mml.domain.entity.Playlist
+
+data class PlaylistDTO(
+        val name: String,
+        val musics: List<MusicDTO>
+) {
+    constructor(playlist: Playlist) : this (
+            name = playlist.name,
+            musics = playlist.musics.map { MusicDTO(it) }
+    )
+}
