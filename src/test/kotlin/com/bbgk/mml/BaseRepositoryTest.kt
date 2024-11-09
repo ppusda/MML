08c7162eb1d0c5510dd27d85cd002a551514d9b7
ppusda <ppusda1234@gmail.com>
2024-11-09 18:19:15 +0900

    feat: 테스트 커버리지 100% 달성하기 - PlaylistMusicRepository / Custom 메서드 테스트

--- src/test/kotlin/com/bbgk/mml/BaseRepositoryTest.kt
+++ src/test/kotlin/com/bbgk/mml/BaseRepositoryTest.kt
@@ -1,13 +1,24 @@
 package com.bbgk.mml
 
 import com.bbgk.mml.domain.entity.Member
+import com.bbgk.mml.domain.entity.Music
+import com.bbgk.mml.domain.entity.Playlist
+import com.bbgk.mml.domain.entity.PlaylistMusic
+import com.bbgk.mml.domain.repository.MemberRepository
+import com.bbgk.mml.domain.repository.MusicRepository
+import com.bbgk.mml.domain.repository.PlaylistMusicRepository
+import com.bbgk.mml.domain.repository.PlaylistRepository
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 
 @DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 abstract class BaseRepositoryTest {
 
     // Id config
@@ -21,10 +32,7 @@ abstract class BaseRepositoryTest {
     protected val DATA_SIZE = 5
     protected val pageable: Pageable = PageRequest.of(PAGE_NUMBER, PAGE_SIZE)
 
-    // Message Config
-    protected val MESSAGE_MUSIC_NOT_EXIST = "존재하지 않는 음악입니다."
-    protected val MESSAGE_PLAYLIST_NOT_EXIST = "존재하지 않는 플레이리스트입니다."
-
     // Member Config
     protected val member = Member("testMember", "1234")
+
 }
---
+++ src/test/kotlin/com/bbgk/mml/domain/repository/PlaylistMusicRepositoryTest.kt
@@ -0,0 +1,64 @@
+package com.bbgk.mml.domain.repository
+
+import com.bbgk.mml.BaseRepositoryTest
+import com.bbgk.mml.domain.entity.Music
+import com.bbgk.mml.domain.entity.Playlist
+import com.bbgk.mml.domain.entity.PlaylistMusic
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class PlaylistMusicRepositoryTest(
+        @Autowired val musicRepository: MusicRepository,
+        @Autowired val playlistRepository: PlaylistRepository,
+        @Autowired val playlistMusicRepository: PlaylistMusicRepository,
+        @Autowired val memberRepository: MemberRepository
+): BaseRepositoryTest() {
+
+    @BeforeAll
+    fun beforeAll() {
+        val musics = mutableListOf(
+                Music("title1", "artist1", "url1"),
+                Music("title2", "artist2", "url2")
+        )
+        val savedMusics = musicRepository.saveAll(musics)
+        val savedUser = memberRepository.save(member)
+
+        val playlist1 = Playlist("playlist1", savedUser)
+        val playlist2 = Playlist("playlist2", savedUser)
+        val savedPlaylists = playlistRepository.saveAll(mutableListOf(playlist1, playlist2))
+
+        playlist1.addMusics(mutableListOf(
+                PlaylistMusic(savedPlaylists[0], savedMusics[0]),
+                PlaylistMusic(savedPlaylists[0], savedMusics[1])
+        ))
+        playlist2.addMusics(mutableListOf(
+                PlaylistMusic(savedPlaylists[1], savedMusics[0]),
+        ))
+
+        val playlistMusic = mutableListOf(
+                PlaylistMusic(savedPlaylists[0], savedMusics[0]),
+                PlaylistMusic(savedPlaylists[0], savedMusics[1]),
+                PlaylistMusic(savedPlaylists[1], savedMusics[0]),
+        )
+
+        playlistMusicRepository.saveAll(playlistMusic)
+    }
+
+    @Test
+    @DisplayName("재생목록 아이디와 음악 아이디로 연관관계 테이블 내 아이디를 검색한다.")
+    fun testFindByPlaylistIdAndMusicId() {
+        // given - beforeAll
+        // when
+        val playlistMusic = playlistMusicRepository.findByPlaylistIdAndMusicId(PLAYLIST_ID, MUSIC_ID)
+
+        // then
+        Assertions.assertThat(playlistMusic).isPresent
+        Assertions.assertThat(playlistMusic.get().id).isEqualTo(PLAYLIST_MUSIC_ID)
+        Assertions.assertThat(playlistMusic.get().playlist.id).isEqualTo(PLAYLIST_ID)
+        Assertions.assertThat(playlistMusic.get().music.id).isEqualTo(MUSIC_ID)
+    }
+
+}
