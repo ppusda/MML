1ea4ea631fec800ecfdda5e8a4fbe8807e1364db
ppusda <ppusda1234@gmail.com>
2024-11-11 03:40:38 +0900

    feat: 테스트 커버리지 100% 달성하기 - MmlApplication main @Generated로 제외

--- src/main/kotlin/com/bbgk/mml/MmlApplication.kt
+++ src/main/kotlin/com/bbgk/mml/MmlApplication.kt
@@ -1,11 +1,13 @@
 package com.bbgk.mml
 
+import lombok.Generated
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class MmlApplication
 
+@Generated
 fun main(args: Array<String>) {
 	runApplication<MmlApplication>(*args)
 }
