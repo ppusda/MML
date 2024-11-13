5743afde27188946aaef850abe49fad661a9b99d
ppusda <ppusda1234@gmail.com>
2024-11-14 02:38:57 +0900

    feat: 간단한 UI 추가하기 - 템플릿 수정 (MusicList)

--- src/main/kotlin/com/bbgk/mml/musicList/controller/MusicListViewController.kt
+++ src/main/kotlin/com/bbgk/mml/musicList/controller/MusicListViewController.kt
@@ -1,19 +1,19 @@
 package com.bbgk.mml.musicList.controller
 
-import com.bbgk.mml.musicList.service.MusicService
+import com.bbgk.mml.musicList.service.PlaylistService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class MusicListViewController(
-        private val musicService: MusicService
+        private val playlistService: PlaylistService
 ) {
 
     @GetMapping("/")
     fun index(model: Model): String {
-        val musics = musicService.getMusics(0)
-        model.addAttribute("musics", musics)
+        val playlists = playlistService.getPlaylists(0)
+        model.addAttribute("playlists", playlists)
 
         return "index"
     }
